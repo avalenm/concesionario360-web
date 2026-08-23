6a8587d88e2ee390e85b0a668bc9aa5e7aee01b3
avalenm <avalenm@users.noreply.github.com>
2026-08-23 02:41:14 +0000

    deploy: avalenm/concesionario360@83b57efd5505aaf045771ba6e36dc5169fbd2036

---
+++ assets/XMarkIcon-Dl3TY9Du.js
@@ -0,0 +1,1 @@
+import{I as e,O as t,T as n}from"./index-nmkyuJkQ.js";function r(r,i){return e(),t(`svg`,{xmlns:`http://www.w3.org/2000/svg`,fill:`none`,viewBox:`0 0 24 24`,"stroke-width":`1.5`,stroke:`currentColor`,"aria-hidden":`true`,"data-slot":`icon`},[n(`path`,{"stroke-linecap":`round`,"stroke-linejoin":`round`,d:`M6 18 18 6M6 6l12 12`})])}export{r as t};
